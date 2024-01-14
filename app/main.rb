f5fd7787a7d3e43cf85db8322089e71efcf7b6e0
Vishless <mail.vishnupk@gmail.com>
2024-01-14 20:53:38 +0530

    Render debug info
    Width: 720
    Height: 640

--- app/main.rb
+++ app/main.rb
@@ -1,8 +1,20 @@
+GRID_SIZE = 20
+
 def handle_input(args); end
 
 def update(args); end
 
-def render(args); end
+def render(args)
+  render_debug_info(args)
+  render_grid(args)
+end
+
+def render_grid(args); end
+
+def render_debug_info(args)
+  args.outputs.labels << [60, 600, "Width: #{args.grid.w}", 1, 1]
+  args.outputs.labels << [60, 570, "Height: #{args.grid.h}", 1, 1]
+end
 
 def tick(args)
   handle_input(args)
